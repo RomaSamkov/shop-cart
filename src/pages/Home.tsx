dc187de2fb71c967868cb0537a195bd04a07df5c
RomaSamkov <Romansamkov.rs@gmail.com>
2023-09-14 16:16:03 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,18 @@
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+import { StoreItem } from "../components/StoreItem";
+
 export function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <h1>Home</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
